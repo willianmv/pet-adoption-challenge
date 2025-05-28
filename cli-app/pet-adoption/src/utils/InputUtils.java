e80942e9ee045f808cac46363d8c35c88850f1af
willianmv <willianvmarques300@gmail.com>
2025-05-28 15:30:35 -0300

    feat(utils): implementa métodos que pedem e validam valores de maneira genérica

--- cli-app/pet-adoption/src/utils/InputUtils.java
+++ cli-app/pet-adoption/src/utils/InputUtils.java
@@ -1,16 +1,18 @@
 package utils;
 
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class InputUtils {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int readIntValue(String message){
-        while (true){
+    public static int readIntValue(String message) {
+        while (true) {
             System.out.println(message);
             String input = scanner.nextLine();
-            try{
+            try {
                 return Integer.parseInt(input);
             } catch (NumberFormatException ex) {
                 System.out.printf("--ENTRADA INVÁLIDA ['%s']. INSIRA UM VALOR DO TIPO INTEIRO.\n", input);
@@ -19,11 +21,11 @@ public class InputUtils {
         }
     }
 
-    public static double readDoubleValue(String message){
-        while (true){
+    public static double readDoubleValue(String message) {
+        while (true) {
             System.out.println(message);
             String input = scanner.nextLine();
-            try{
+            try {
                 return Double.parseDouble(input);
             } catch (NumberFormatException ex) {
                 System.out.printf("--ENTRADA INVÁLIDA ['%s']. INSIRA UM VALOR DO TIPO DOUBLE.\n", input);
@@ -32,13 +34,38 @@ public class InputUtils {
         }
     }
 
-    public  static String readNotEmptyString(String message){
-        while (true){
+    public static String readNotEmptyString(String message) {
+        while (true) {
             System.out.println(message);
             String input = scanner.nextLine().trim();
-            if(!input.isEmpty()) return input;
+            if (!input.isEmpty()) return input;
             System.out.printf("--ENTRADA INVÁLIDA ['%s']. ENTRE COM UMA STRING NÃO VAZIA\n", input);
         }
     }
 
+    //passar uma interface funcional de supplier pq n recebe argumentos e executa uma função q retorna algo
+    //passar uma interface funcional de function pq recebe um valor e retorna um valor
+    public static <T> T readAndValidatedValue(Supplier<T> inputReader, Function<T, T> validator) {
+        while (true) {
+            try {
+                T input = inputReader.get();
+                return validator.apply(input);
+            } catch (RuntimeException ex) {
+                System.out.println("--ERRO: " + ex.getMessage());
+            }
+        }
+    }
+
+    //aqui tem que receber uma function que recebe um tipo e retorna outro pra transformar string validada em enum
+    public static <T, R> R readAndTransformValue(Supplier<T> inputReader, Function<T, R> transformer) {
+        while (true) {
+            try {
+                T input = inputReader.get();
+                return transformer.apply(input);
+            } catch (RuntimeException ex) {
+                System.out.println("--ERRO: " + ex.getMessage());
+            }
+
+        }
+    }
 }
