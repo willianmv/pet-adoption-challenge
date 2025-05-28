acb3de0e2b5c007e92b331158324462bd994a0f7
willianmv <willianvmarques300@gmail.com>
2025-05-28 15:28:19 -0300

    refactor(pet-validator): melhora mensagens retornadas pelas exceções

--- cli-app/pet-adoption/src/utils/PetValidator.java
+++ cli-app/pet-adoption/src/utils/PetValidator.java
@@ -6,21 +6,21 @@ public class PetValidator {
 
     public static String validatePetStringFields(String name){
         if(!name.matches("^[A-Za-zÀ-ÿ\\s]+$")){
-            throw new InvalidPetDataException("O campo só pode conter letras.");
+            throw new InvalidPetDataException("O CAMPO SÓ PODE CONTER LETRAS.");
         }
         return name.trim();
     }
 
     public static double validateWeight(double weight){
         if(weight < 0.5 || weight > 60){
-            throw new InvalidPetDataException("O peso deve estar entre 0.5kg e 60kg.");
+            throw new InvalidPetDataException("O PESO DEVE SER ENTRE 0.5kg E 60kg.");
         }
         return weight;
     }
 
     public static double validateAge(double age){
         if(age > 20){
-            throw new InvalidPetDataException("A idade não pode ser maior que 20 anos.");
+            throw new InvalidPetDataException("A IDADE NÃO PODE SER MAIOR DE 20 ANOS.");
         }
         return age;
     }
